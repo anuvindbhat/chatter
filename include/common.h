569a1615da5cbc5296b816832d4f7064829c6d0f
anuvindbhat <anuvind.bhat@gmail.com>
2019-02-17 19:25:58 +0530

    misc

--- include/common.h
+++ include/common.h
@@ -9,8 +9,9 @@ Copyright (c) 2019 Anuvind Bhat
 // #define DEBUG
 
 #define PORT_NUMBER 1729
+// Includes '\0' character
 #define MSG_SIZE 1000
-#define NAME_SIZE 15
+#define NAME_SIZE 16
 
 void start_chat(int sockfd, char *name);
 
